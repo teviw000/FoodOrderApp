6703a635ecb5b1217485db41d7d4ac872dbe88a9
teviw000 <31836504+teviw000@users.noreply.github.com>
2021-06-10 15:02:10 -0500

    Finished shopping cart component and added a modal for the total amount for checkout

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import Card from '../../UI/Card/Card';
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
@@ -12,7 +13,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                
+                <MealItemForm />
             </div>
         </li>
     )
