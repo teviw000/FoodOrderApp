e07e0b242fdba642c0523462ff30c86faab135c3
teviw000 <31836504+teviw000@users.noreply.github.com>
2021-06-12 21:02:43 -0500

    Added refs from inputs to cart components. Can now update. Also added in Context API

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,35 @@
+import { useRef, useState } from "react";
+
 import Input from "../../UI/Input/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountisValid, setAmountIsValid] = useState(true);
+
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value; // this will return a string back to us
+    const enteredAmountNum = +enteredAmount; //converstion of string to number
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNum < 1 ||
+      enteredAmountNum > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    
+    props.onAddToCart(enteredAmountNum);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount" + props.id,
@@ -16,6 +41,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountisValid && <p>Please enter a valid amount (1-5)</p>}
     </form>
   );
 };
