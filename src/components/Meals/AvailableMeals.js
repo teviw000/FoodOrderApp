a0140dabfd6a4dfdd6c87443261c226aea619c14
teviw000 <31836504+teviw000@users.noreply.github.com>
2021-07-07 15:35:59 -0500

    Added error handing for http requests by implementing catch method for responses from fetchMeals call

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,14 +5,20 @@ import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const [meals, setMeals] = useState([])
+  const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://food-app-e251f-default-rtdb.firebaseio.com/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -25,13 +31,16 @@ const AvailableMeals = () => {
           price: responseData[key].price,
         });
       }
-      
+
       setMeals(loadedMeals);
       setIsLoading(false);
     };
 
-    fetchMeals();
-  }, []); 
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
 
   const mealsList = meals.map((meal) => (
     <MealItem
@@ -43,11 +52,21 @@ const AvailableMeals = () => {
     />
   ));
 
-    if (isLoading) {
-      return <section className={classes.mealsLoading} >
+  if (isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
         <p>Loading</p>
       </section>
-    }
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.mealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
 
   return (
     <section className={classes.meals}>
